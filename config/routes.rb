7546847f971d4db810954784318af168998df7b2
shumasaito <shushushu1205@gmail.com>
2025-04-04 09:42:54 +0000

    [Finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     root to: "homes#top"
     get 'homes/about' => 'homes#about', as: 'about'
   resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
