18eeef62824161bc5425fc38d2595e183db2197c
shumasaito <shushushu1205@gmail.com>
2025-04-04 08:26:46 +0000

    [Finish]4-18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -22,7 +22,7 @@ class PostImagesController < ApplicationController
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
-    render index
+    redirect_to post_images_path
   end
   
   private
