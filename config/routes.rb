ce9b8cea83c2d5e5807435f8dd24dd8633a511b4
shumasaito <shushushu1205@gmail.com>
2025-05-08 05:56:19 +0000

    [Add]構成の改善

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+  
+  scope module: :public do
+    devise_for :users
+    root to: "homes#top"
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+  end
+  
+end
